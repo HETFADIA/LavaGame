dce9fd9c5422a72d16988d321eed9073da47c222
Het <62541263+HETFADIA@users.noreply.github.com>
2021-07-06 04:32:21 +0530

    multi update

--- Debugging/js/Multi.js
+++ Debugging/js/Multi.js
@@ -57,7 +57,17 @@ function bfs_monster(monsters,dp,s){
         lvl++;
         t=q.length;
         for(var i=0;i<t;i++){
-            
+            var items=q.dequeue();
+            a=items[0]
+            b=items[1]
+            if(dp[a][b]>lvl){
+              dp[a][b]=lvl
+              if(a-1>=0 && !visited[a-1][b] && s[a-1][b]!='#') q.enqueue([a-1,b]);
+              if(b-1>=0 && !visited[a][b-1] && s[a][b-1]!='#') q.enqueue([a,b-1]);
+              if(a+1<n && !visited[a+1][b] && s[a+1][b]!='#') q.enqueue([a+1,b]);
+              if(b+1<m && !visited[a][b+1] && s[a][b+1]!='#') q.enqueue([a,b+1]);
+              visited[a][b]=true;
+            }
         }
     }
 
